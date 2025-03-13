3058440d661dcd853136c4304ea7e5fb3f87200d
Matthew Trudeau <matt@yumaai.com>
2025-03-13 15:58:15 -0400

    First attempt at aborting merge from pulling

--- precog/validators/scripts/auto_updater.py
+++ precog/validators/scripts/auto_updater.py
@@ -6,6 +6,12 @@ import bittensor as bt
 import git
 
 import precog
+from precog.utils.timestamp import elapsed_seconds, get_now
+from git.exc import GitCommandError
+
+# Frequency of the auto updater in minutes
+TIME_INTERVAL = 5
+
 
 
 def git_pull_change(path, max_retries=3, retry_delay=5) -> bool:
@@ -13,12 +19,9 @@ def git_pull_change(path, max_retries=3, retry_delay=5) -> bool:
     repo = git.Repo(path)
     current_hash = repo.head.commit
 
-    # Kill auto update if there are local changes
+    # Notify of observed local changes
     if repo.is_dirty():
         bt.logging.debug("Local changes detected.")
-        bt.logging.debug("Only run auto update if there are no local changes.")
-        bt.logging.debug("Killing the auto updater.")
-        raise RuntimeError("Local changes detected. Auto update killed")
 
     # Try pulling with retries
     for attempt in range(max_retries):
@@ -27,6 +30,24 @@ def git_pull_change(path, max_retries=3, retry_delay=5) -> bool:
             repo.remotes.origin.pull(rebase=True)
             bt.logging.debug("Pull complete.")
             break
+
+        except GitCommandError as e:
+            if "merge conflicts" in str(e):
+                bt.logging.debug("Merge conflicts detected. Reverting to the previous state.")
+                repo.git.merge('--abort')
+                bt.logging.debug("Stopping the pm2 process for the auto updater.")
+                return
+
+            else:
+                bt.logging.debug(f"An error occurred: {e}")
+                if attempt < max_retries - 1:  # Don't sleep on the last attempt
+                    bt.logging.debug(f"Pull attempt {attempt + 1} failed: {str(e)}")
+                    bt.logging.debug(f"Retrying in {retry_delay} seconds...")
+                    time.sleep(retry_delay)
+                else:
+                    bt.logging.debug(f"All pull attempts failed. Last error: {str(e)}")
+                    raise  # Re-raise the last exception if all retries failed
+
         except Exception as e:
             if attempt < max_retries - 1:  # Don't sleep on the last attempt
                 bt.logging.debug(f"Pull attempt {attempt + 1} failed: {str(e)}")
