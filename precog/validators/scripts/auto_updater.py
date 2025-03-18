f414afb141c01d75e24a3e7c1ce4705f82ea798c
Matthew Trudeau <matt@yumaai.com>
2025-03-18 13:10:48 -0400

    Attempt using built in pm2 cron

--- precog/validators/scripts/auto_updater.py
+++ precog/validators/scripts/auto_updater.py
@@ -58,50 +58,58 @@ if __name__ == "__main__":
     with pkg_resources.path(precog, "..") as p:
         git_repo_path = p
 
-    # Loop until we observe github activity
-    while True:
+    # # Loop until we observe github activity
+    # while True:
 
-        # Get current timestamp
-        now = get_now()
+    #     # Get current timestamp
+    #     now = get_now()
 
-        # Check if the current minute is 2 minutes past anticipated validator query time
-        if now.minute % TIME_INTERVAL == 2:
+    #     # Check if the current minute is 2 minutes past anticipated validator query time
+    #     if now.minute % TIME_INTERVAL == 2:
 
-            bt.logging.debug("Checking for repository changes...")
+    bt.logging.debug("Checking for repository changes...")
 
-            # Pull the latest changes from github
-            has_changed = git_pull_change(git_repo_path)
+    # Pull the latest changes from github
+    has_changed = git_pull_change(git_repo_path)
 
-            # If the repo has changed, break the loop
-            if has_changed:
-                bt.logging.debug("Repository has changed!")
-                break
+    # If the repo has changed, break the loop
+    if has_changed:
+        bt.logging.debug("Repository has changed!")
+        # break
 
-            # If the repo has not changed, sleep
-            else:
-                bt.logging.debug("Repository has not changed. Sleep mode activated.")
+        # This code is only reached when the repo has changed
+        # We can now restart both pm2 processes, including the auto updater
+        # Let the script simply end and the new process will be restarted by pm2
+        bt.logging.debug("Installing dependencies...")
+        subprocess.run(["poetry", "install"], cwd=git_repo_path)
+        bt.logging.debug("Restarting pm2 processes...")
+        subprocess.run(["pm2", "restart", "app.config.js"], cwd=git_repo_path)
 
-                # Calculate the time of the next git pull check
-                next_check = now + timedelta(minutes=TIME_INTERVAL)
-                next_check = next_check.replace(second=0)
+    # If the repo has not changed, sleep
+    else:
+        bt.logging.debug("Repository has not changed. Sleep mode activated.")
 
-                # Determine the number of seconds to sleep
-                seconds_to_sleep = elapsed_seconds(get_now(), next_check)
+        #         # Calculate the time of the next git pull check
+        #         next_check = now + timedelta(minutes=TIME_INTERVAL)
+        #         next_check = next_check.replace(second=0)
 
-                # Sleep for the exact number of seconds to the next git pull check
-                time.sleep(seconds_to_sleep)
-        else:
+        #         # Determine the number of seconds to sleep
+        #         seconds_to_sleep = elapsed_seconds(get_now(), next_check)
 
-            # Sleep for 45 seconds
-            # This is to prevent the script from checking for changes too frequently
-            # This specific `else` block should not be reach too often since we sleep for the exact time of the anticipated validator query time
-            bt.logging.debug("Sleeping for 45 seconds")
-            time.sleep(45)
+        #         # Sleep for the exact number of seconds to the next git pull check
+        #         time.sleep(seconds_to_sleep)
+        # else:
 
-    # This code is only reached when the repo has changed
-    # We can now restart both pm2 processes, including the auto updater
-    # Let the script simply end and the new process will be restarted by pm2
-    bt.logging.debug("Installing dependencies...")
-    subprocess.run(["poetry", "install"], cwd=git_repo_path)
-    bt.logging.debug("Restarting pm2 processes...")
-    subprocess.run(["pm2", "restart", "app.config.js"], cwd=git_repo_path)
+        #     # Sleep for 45 seconds
+        #     # This is to prevent the script from checking for changes too frequently
+        #     # This specific `else` block should not be reach too often since we sleep for the exact time of the anticipated validator query time
+        #     bt.logging.debug("Sleeping for 45 seconds")
+        #     time.sleep(45)
+
+    # # This code is only reached when the repo has changed
+    # # We can now restart both pm2 processes, including the auto updater
+    # # Let the script simply end and the new process will be restarted by pm2
+    # bt.logging.debug("Installing dependencies...")
+    # subprocess.run(["poetry", "install"], cwd=git_repo_path)
+    # bt.logging.debug("Restarting pm2 processes...")
+    # subprocess.run(["pm2", "restart", "app.config.js"], cwd=git_repo_path)
