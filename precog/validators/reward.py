715ef7e415adeb408aaf6f641c503d9cbd62815a
Peter | Yuma, a DCG Company <peter@yumaai.com>
2025-05-19 09:46:45 -0400

    Dev (#116) (#117)
    * Update rewards to better handle ties (#109)
    
    * Update Staging (#100) (#101)
    
    * Enhancement/increased evaluation window (#91)
    
    * update the rewards to be calculated with the last 6 hours of predictions
    
    * make evaluation window a config option
    
    * update reward to properly get 6 hours of past data offset by one hour ago.
    
    * add completeness to rewards calculation
    
    * Just multiply the current rewards by the completeness score
    
    * remove config options and introduce constants for the evaluation window
    
    * release notes and version incremementing
    
    ---------
    
    * add function to handle tied weights
    
    * updated implementation
    
    * np nonzero instead of where
    
    * correct fn name
    
    ---------
    
    * update version (#110)
    
    * Rewards Changes (#111)
    
    * Update rewards to better handle ties (#109)
    
    * Update Staging (#100) (#101)
    
    * Enhancement/increased evaluation window (#91)
    
    * update the rewards to be calculated with the last 6 hours of predictions
    
    * make evaluation window a config option
    
    * update reward to properly get 6 hours of past data offset by one hour ago.
    
    * add completeness to rewards calculation
    
    * Just multiply the current rewards by the completeness score
    
    * remove config options and introduce constants for the evaluation window
    
    * release notes and version incremementing
    
    ---------
    
    * add function to handle tied weights
    
    * updated implementation
    
    * np nonzero instead of where
    
    * correct fn name
    
    ---------
    
    * update version (#110)
    
    ---------
    
    * Update date (#113)
    
    * Ensure intervals are evaluated across an hour of prices
    
    ---------
    
    Co-authored-by: Matt | Yuma | DCG <173096484+matt-yuma@users.noreply.github.com>

--- precog/validators/reward.py
+++ precog/validators/reward.py
@@ -95,6 +95,8 @@ def calc_rewards(
 
     point_weights = get_average_weights_for_ties(point_ranks, decay)
     interval_weights = get_average_weights_for_ties(interval_ranks, decay)
+    bt.logging.trace(f"point_weights: {point_weights}")
+    bt.logging.trace(f"interval_weights: {point_weights}")
 
     base_rewards = (point_weights + interval_weights) / 2
     rewards = base_rewards * np.array(completeness_scores)
