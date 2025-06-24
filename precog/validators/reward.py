fb40431b4f148790219a047c2264d76fe9795ca7
Peter | Yuma, a DCG Company <peter@yumaai.com>
2025-06-24 13:50:36 -0400

    Add release notes (#121)
    * Add reward logging (#118)
    
    Co-authored-by: Matt | Yuma | DCG <173096484+matt-yuma@users.noreply.github.com>
    
    * add log for base_reward
    
    ---------
    
    Co-authored-by: Matt | Yuma | DCG <173096484+matt-yuma@users.noreply.github.com>
    
    * update version and release notes
    
    * mend
    
    ---------
    
    Co-authored-by: Matt | Yuma | DCG <173096484+matt-yuma@users.noreply.github.com>

--- precog/validators/reward.py
+++ precog/validators/reward.py
@@ -139,7 +139,10 @@ def calc_rewards(
 
     point_weights = get_average_weights_for_ties(point_ranks, decay)
     interval_weights = get_average_weights_for_ties(interval_ranks, decay)
+    bt.logging.trace(f"point_weights: {point_weights}")
+    bt.logging.trace(f"interval_weights: {point_weights}")
 
     rewards = (point_weights + interval_weights) / 2
+    bt.logging.trace(f"base_rewards: {rewards}")
 
     return rewards
